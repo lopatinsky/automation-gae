67cb0b1552526f6211602012e3e4a71e5fe217f2
Ilya Zorin <geraltencore@gmail.com>
2014-09-30 18:33:27 +0400

    indexed property and order of classes fixed

--- models/models.py
+++ models/models.py
@@ -13,57 +13,44 @@ READY_ORDER = 1
 CANCELED_BY_CLIENT_ORDER = 2
 CANCELED_BY_BARISTA_ORDER = 3
 
-#TODO no sure
+#TODO not sure
 PAYMENT_SUCCESS = 1
 PAYMENT_FAIL = 0
 
 IOS_DEVICE = 0
 ANDROID_DEVICE = 1
 
-class Venue(ndb.Model):
-    venue_id = ndb.IntegerProperty(required=True)
-    title = ndb.StringProperty(required=True)
-    description = ndb.StringProperty()
-    pic = ndb.StringProperty()
-    coordinates = ndb.GeoPtProperty(required=True)
-    #TODO better solution than string?
-    working_days = ndb.StringProperty()
-    working_hours = ndb.StringProperty()
-    menu = ndb.KeyProperty(kind=MenuCategory, repeated=True)
-
-class Client(ndb.Model):
-    client_id = ndb.IntegerProperty(required=True)
-    name = ndb.StringProperty(required=True)
-    tel = ndb.StringProperty(required=True)
-
 class MenuCategory(ndb.Model):
     category_id = ndb.IntegerProperty(required=True)
-    title = ndb.StringProperty(required=True)
-    picture = ndb.StringProperty()
-    menu_items = ndb.KeyProperty(kind=MenuItem, repeated=True)
+    title = ndb.StringProperty(required=True, indexed=False)
+    picture = ndb.StringProperty(indexed=False)
+    menu_items = ndb.KeyProperty(kind=MenuItem, repeated=True, indexed=False)
 
 class MenuItem(ndb.Model):
     menu_item_id = ndb.IntegerProperty(required=True)
-    title = ndb.StringProperty(required=True)
-    description = ndb.StringProperty()
-    picture = ndb.StringProperty()
-    kal = ndb.IntegerProperty()
-    price = ndb.IntegerProperty(required=True)
-    cost_price = ndb.IntegerProperty()
+    title = ndb.StringProperty(required=True, indexed=False)
+    description = ndb.StringProperty(indexed=False)
+    picture = ndb.StringProperty(indexed=False)
+    kal = ndb.IntegerProperty(indexed=False)
+    price = ndb.IntegerProperty(required=True, indexed=False)
+    cost_price = ndb.IntegerProperty(indexed=False)
     status = ndb.IntegerProperty(required=True, choices=(STATUS_AVAILABLE, STATUS_UNAVAILABLE),
                                  default=STATUS_AVAILABLE)
 
-class PaymentType(ndb.Model):
-    payment_id = ndb.IntegerProperty(required=True)
-    title = ndb.StringProperty()
-    status = ndb.IntegerProperty(required=True, choices=(STATUS_AVAILABLE, STATUS_UNAVAILABLE),
-                                 default=STATUS_AVAILABLE)
+class Venue(ndb.Model):
+    venue_id = ndb.IntegerProperty(required=True)
+    title = ndb.StringProperty(required=True, indexed=False)
+    description = ndb.StringProperty(indexed=False)
+    pic = ndb.StringProperty(indexed=False)
+    coordinates = ndb.GeoPtProperty(required=True, indexed=False)
+    working_days = ndb.StringProperty(indexed=False)
+    working_hours = ndb.StringProperty(indexed=False)
+    menu = ndb.KeyProperty(kind=MenuCategory, repeated=True, indexed=False)
 
 class Order(ndb.Model):
     order_id = ndb.IntegerProperty(required=True)
     client_id = ndb.IntegerProperty(required=True)
-    #TODO can be calculated from menu items or nevermind?
-    total_sum = ndb.IntegerProperty()
+    total_sum = ndb.IntegerProperty(indexed=False)
     status = ndb.IntegerProperty(required=True, choices=(NEW_ORDER, READY_ORDER, CANCELED_BY_CLIENT_ORDER,
                                                          CANCELED_BY_BARISTA_ORDER),
                                  default=NEW_ORDER)
@@ -71,18 +58,27 @@ class Order(ndb.Model):
     delivery_time = ndb.TimeProperty(required=True)
     payment_type_id = ndb.IntegerProperty(required=True, choices=(CASH_PAYMENT_TYPE, CARD_PAYMENT_TYPE))
     payment_status = ndb.IntegerProperty(choices=(PAYMENT_SUCCESS, PAYMENT_FAIL))
-    coordinates = ndb.GeoPtProperty()
+    coordinates = ndb.GeoPtProperty(indexed=False)
     venue_id = ndb.IntegerProperty(required=True)
-    pan = ndb.StringProperty()
+    pan = ndb.StringProperty(indexed=False)
     #TODO whats the difference?
-    return_comment = ndb.StringProperty()
-    comment = ndb.StringProperty()
-    return_datetime = ndb.DateTimeProperty()
+    return_comment = ndb.StringProperty(indexed=False)
+    comment = ndb.StringProperty(indexed=False)
+    return_datetime = ndb.DateTimeProperty(indexed=False)
     #TODO alpha order id maybe?
     payment_id = ndb.StringProperty()
     device_type = ndb.IntegerProperty(required=True)
 
+class Client(ndb.Model):
+    client_id = ndb.IntegerProperty(required=True)
+    name = ndb.StringProperty(required=True, indexed=False)
+    tel = ndb.StringProperty(required=True, indexed=False)
 
+class PaymentType(ndb.Model):
+    payment_id = ndb.IntegerProperty(required=True)
+    title = ndb.StringProperty(indexed=False)
+    status = ndb.IntegerProperty(required=True, choices=(STATUS_AVAILABLE, STATUS_UNAVAILABLE),
+                                 default=STATUS_AVAILABLE, indexed=False)
 
 
 
