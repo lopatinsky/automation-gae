d2ffff172c529385c46d128295923aefb8ceeef5
Ilya Zorin <geraltencore@gmail.com>
2014-10-27 20:02:55 +0300

    extended status endpoint final version

--- methods/alfa_bank.py
+++ methods/alfa_bank.py
@@ -4,6 +4,8 @@ import urllib
 from google.appengine.api import urlfetch
 from config import config
 
+ALPHA_CARD_LIMIT_CODES = [-20010, 902, 116, 123]
+CARD_LIMIT_CODE = 1
 
 def __post_request_alfa(api_path, params):
     url = '%s%s' % (config.ALFA_BASE_URL, api_path)
@@ -47,7 +49,11 @@ def check_extended_status(order_id):
     }
     result = __post_request_alfa('/rest/getOrderStatusExtended.do', params)
     result_json = json.loads(result)
-    return {'error_code': result_json['actionCode'],
+    if result_json['actionCode'] in ALPHA_CARD_LIMIT_CODES:
+        status_code = CARD_LIMIT_CODE
+    else:
+        status_code = result_json['actionCode']
+    return {'error_code': status_code,
             'description': result_json['actionCodeDescription']}
 
 def get_back_blocked_sum(order_id):
