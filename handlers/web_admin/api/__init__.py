5ebeb51c5d03b3189ad3a276dbaf74c475b6c380
Mikhail Burshteyn <mdburshteyn@gmail.com>
2014-11-20 21:28:56 +0300

    Remove unneeded TODO

--- handlers/web_admin/api/__init__.py
+++ handlers/web_admin/api/__init__.py
@@ -1,12 +1,1 @@
-# TODO
-# admin api handlers:
-# - auth.php
-# + check_time.php
-# + check_update.php
-# + done.php
-# - register_user.php
-# + return_barista.php
-# + status_up.php
-
-
 from .orders import CheckTimeHandler, CheckUpdateHandler, OrderCancelHandler, OrderDoneHandler, OrderStatusUpdateHandler
