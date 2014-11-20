40e1d2d1bb9acab33ea208a1d2086b1feae4de01
Mikhail Burshteyn <mdburshteyn@gmail.com>
2014-11-20 21:28:40 +0300

    Pay for the order on cron, move cron to 7am

--- handlers/cron/close_orders.py
+++ handlers/cron/close_orders.py
@@ -1,13 +1,21 @@
-from google.appengine.ext import ndb
+import logging
 from handlers.api.base import ApiHandler
-from models import Order, NEW_ORDER, READY_ORDER
-
-__author__ = 'ilyazorin'
+from methods import alfa_bank, empatika_promos
+from models import Order, NEW_ORDER, READY_ORDER, CARD_PAYMENT_TYPE
 
 
 class CloseOpenedOrdersHandler(ApiHandler):
     def get(self):
         orders = Order.query(Order.status == NEW_ORDER).fetch()
         for order in orders:
+            logging.info("closing order %s", order.key.id())
+            if order.payment_type_id == CARD_PAYMENT_TYPE:
+                alfa_bank.pay_by_card(order.payment_id, 0)  # TODO check success
+                if order.mastercard:
+                    points = len(order.items)
+                    try:
+                        empatika_promos.register_order(order.client_id, points, order.key.id())
+                    except empatika_promos.EmpatikaPromosError as e:
+                        logging.exception(e)
             order.status = READY_ORDER
-        ndb.put_multi(orders)
+            order.put()
