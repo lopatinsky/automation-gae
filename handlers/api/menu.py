0545e120d0b764e194f7b86c7f63a8f5a85c3e33
Mikhail Burshteyn <mdburshteyn@gmail.com>
2014-11-23 15:26:21 +0300

    Check and possibly generate new client_id on menu endpoint

--- handlers/api/menu.py
+++ handlers/api/menu.py
@@ -7,16 +7,21 @@ __author__ = 'ilyazorin'
 class MenuHandler(ApiHandler):
 
     def get(self):
-        client_id = self.request.get('client_id')
+        client_id = self.request.get_range('client_id')
         categories = MenuCategory.query().fetch()
         result_dict = {}
         for category in categories:
             result_dict.update(category.dict())
         response = {'menu': result_dict}
-        if not client_id:
+
+        client = None
+        try:
+            client = Client.get_by_id(client_id)
+        except:
+            pass
+        if not client:
             client = Client.create()
             client.put()
-            response['client_id'] = client.key.id()
-        else:
-            response['client_id'] = client_id
+
+        response['client_id'] = client.key.id()
         self.render_json(response)
