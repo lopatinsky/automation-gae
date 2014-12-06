341d04fd00260f5d7f92fc8ed6b571019cc52e88
dvpermyakov <dvpermyakov@edu.hse.ru>
2014-12-07 02:34:30 +0300

    fix select venue and add year, month and date to filter query, so it is whole for clients report

--- handlers/maintenance/report.py
+++ handlers/maintenance/report.py
@@ -1,6 +1,11 @@
+__author__ = 'dvpermyakov'
+
 from .base import BaseHandler
 from models import Order, Client, Venue
-import logging
+from datetime import datetime
+
+PROJECT_STARTING_YEAR = 2013
+MONTHS_NUMBER = range(1, 12 + 1)
 
 class ReportHandler(BaseHandler):
     def get(self):
@@ -21,26 +26,41 @@ class ReportedClient:
 
 class ClientReportHandler(BaseHandler):
 
-    def clientsTable(self, venue_id):
-        orders = Order.query().fetch()
-        map = {}  # TODO rename
+    def clientsTable(self, venue_id, chosen_year, chosen_month, chosen_day):
+        orders = Order.query(Order.venue_id == venue_id).fetch() if venue_id != 0 else Order.query().fetch()
+        clients = {}
         total_number = 0
         total_cost = 0
         for order in orders:
-            if order.venue_id != venue_id:  # TODO query only required venue
+            if order.delivery_time.year != chosen_year and chosen_year != 0: # TODO erase this bidlo-code
                 continue
-            id = order.client_id
-            if id in map:
-                map[id].addOrder(order.total_sum)
+            if order.delivery_time.month != chosen_month and chosen_month != 0: # TODO erase and this bidlo-code
+                continue
+            if order.delivery_time.day != chosen_day and chosen_day != 0: # TODO erase and this bidlo-code
+                continue
+            client_id = order.client_id
+            if client_id in clients:
+                clients[client_id].addOrder(order.total_sum)
             else:
-                client = Client.get_by_id(id)
-                map[id] = ReportedClient(id, client.name, client.tel, order.total_sum)
+                client = Client.get_by_id(client_id)
+                clients[client_id] = ReportedClient(client_id, client.name, client.tel, order.total_sum)
             total_number = total_number + 1
             total_cost = total_cost + order.total_sum
-        return map, total_number, total_cost
+        return clients, total_number, total_cost
 
     def get(self):
-        venue_id = self.request.get_range("selectedVenue")
-        clients_info = self.clientsTable(venue_id)  # TODO assign to three names
-        self.render('clients.html', venues=Venue.query().fetch(), clients=clients_info[0].values(),
-                    venue_number=clients_info[1], venue_expenditure=clients_info[2], current_venue=Venue.get_by_id(venue_id))
+        # get selected_venue == 0 if choose all venues
+        # get selected_year == 0 if choose all years
+        # get selected_month == 0 if choose all months
+        # get selected_day == 0 if choose all days
+        venue_id = self.request.get_range("selected_venue")
+        chosen_year = self.request.get_range("selected_year")
+        chosen_month = self.request.get_range("selected_month")
+        chosen_day = self.request.get_range("selected_day")
+
+        clients, venue_total_number, venue_total_cost = self.clientsTable(venue_id, chosen_year, chosen_month, chosen_day)
+        chosen_venue = Venue.get_by_id(venue_id) if venue_id else None
+        self.render('clients.html', venues=Venue.query().fetch(), clients=clients.values(),
+                    venue_number=venue_total_number, venue_expenditure=venue_total_cost, chosen_venue=chosen_venue,
+                    start_year=PROJECT_STARTING_YEAR, end_year=datetime.now().year, chosen_year=chosen_year,
+                    chosen_month=chosen_month, chosen_day=chosen_day)
