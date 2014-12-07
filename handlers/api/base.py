fe16ede2f44dc392ca10ab610a6d3afeea2c6b9f
Mikhail Burshteyn <mdburshteyn@gmail.com>
2014-12-07 16:08:23 +0300

    Fixes for iOS 1.1 fuckup

--- handlers/api/base.py
+++ handlers/api/base.py
@@ -1,10 +1,27 @@
 import json
 import logging
+from google.appengine.api import urlfetch
 import webapp2
+from config import config
 
 
 class ApiHandler(webapp2.RequestHandler):
     def dispatch(self):
+        # TODO iOS 1.1 fuckup
+        if config.DEBUG and self.request.headers['User-Agent'].startswith("DoubleB/1.1 "):
+            result = urlfetch.fetch(
+                'http://empatika-doubleb.appspot.com' + self.request.path_qs,
+                self.request.body,
+                self.request.method,
+                self.request.headers,
+                deadline=55
+            )
+            self.response.status_int = result.status_code
+            self.response.headers['Content-Type'] = result.headers['Content-Type']
+            self.response.write(result.content)
+            return
+        # TODO end
+
         for key, value in self.request.POST.iteritems():
             if key == "password":
                 value = "(VALUE HIDDEN)"
--- handlers/api/menu.py
+++ handlers/api/menu.py
@@ -1,7 +1,24 @@
+import logging
 from handlers.api.base import ApiHandler
 from models import MenuCategory, Client, STATUS_AVAILABLE
 
 __author__ = 'ilyazorin'
+
+
+_FUCKUP_CLIENTS_MAP = {
+    200052: 200126,
+    200053: 200500,
+    200055: 200646,
+    200056: 200252,
+    200059: 200188,
+    200060: 200194,
+    200061: 200726,
+    200062: 200448,
+    200063: 200312,
+    200064: 200489,
+    200065: 200759,
+    200066: 200377
+}
 
 
 class MenuHandler(ApiHandler):
@@ -13,6 +30,16 @@ class MenuHandler(ApiHandler):
         for category in categories:
             result_dict.update(category.dict())
         response = {'menu': result_dict}
+
+        # TODO iOS 1.1 fuckup
+        if client_id in _FUCKUP_CLIENTS_MAP:
+            logging.warning("fuckup found: %s", client_id)
+            client_id = _FUCKUP_CLIENTS_MAP[client_id]
+
+        if client_id == 200057 and 'Android' not in self.request.headers['User-Agent']:
+            logging.warning("fuckup found: 200057")
+            client_id = 0
+        # TODO END
 
         client = None
         try:
