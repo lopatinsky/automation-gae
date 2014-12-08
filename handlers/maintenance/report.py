4ea88505fcafad65e0b113cd9309b6175a1548c9
dvpermyakov <dvpermyakov@edu.hse.ru>
2014-12-08 20:00:48 +0300

    first version of payment and client total sum

--- handlers/maintenance/report.py
+++ handlers/maintenance/report.py
@@ -1,7 +1,7 @@
 __author__ = 'dvpermyakov'
 
 from .base import BaseHandler
-from models import Order, Client, Venue, MenuItem, READY_ORDER
+from models import Order, Client, Venue, MenuItem, READY_ORDER, BONUS_PAYMENT_TYPE
 from datetime import datetime
 
 PROJECT_STARTING_YEAR = 2013
@@ -17,17 +17,19 @@ def correspond_date(date, day, month, year):
     return True
 
 class ReportedClient:
-    def __init__(self, id, name, tel, orderSum):
+    def __init__(self, id, name, tel, order_sum, payment):
         self.id = id
         self.name = name
         self.tel = tel
-        self.amountOrders = 1
-        self.totalSum = orderSum
-        self.averageOrderCost = orderSum
-    def addOrder(self, orderSum):
-        self.amountOrders += 1
-        self.totalSum += orderSum
-        self.averageOrderCost = self.totalSum / self.amountOrders
+        self.amount_orders = 1
+        self.total_sum = order_sum
+        self.payment = payment
+        self.average_order_cost = order_sum
+    def addOrder(self, order_sum, payment):
+        self.amount_orders += 1
+        self.total_sum += order_sum
+        self.payment += payment
+        self.average_order_cost = self.total_sum / self.amount_orders
 
 class ClientsReportHandler(BaseHandler):
 
@@ -36,6 +38,7 @@ class ClientsReportHandler(BaseHandler):
         clients = {}
         total_number = 0
         total_cost = 0
+        total_payment = 0
         query = Order.query(Order.status == READY_ORDER)
         if venue_id != 0:
             query = query.filter(Order.venue_id == venue_id)
@@ -44,14 +47,17 @@ class ClientsReportHandler(BaseHandler):
                 continue
             client_id = order.client_id
             if client_id in clients:
-                clients[client_id].addOrder(order.total_sum)
+                clients[client_id].addOrder(order.total_sum, payment)
             else:
                 client = Client.get_by_id(client_id)
-                clients[client_id] = ReportedClient(client_id, client.name, client.tel, order.total_sum)
+                total_sum = sum(item.get().price for item in order.items)
+                payment = order.total_sum if order.payment_type_id != BONUS_PAYMENT_TYPE else 0
+                clients[client_id] = ReportedClient(client_id, client.name, client.tel, total_sum, payment)
 
-            total_number += 1
-            total_cost += order.total_sum
-        return clients, total_number, total_cost
+        return clients,\
+               sum(client.amount_orders for client in clients.values()),\
+               sum(client.total_sum for client in clients.values()),\
+               sum(client.payment for client in clients.values())
 
     def get(self):
         # selected_*param == 0 if choose all *pararm
@@ -60,13 +66,14 @@ class ClientsReportHandler(BaseHandler):
         chosen_month = self.request.get_range("selected_month")
         chosen_day = self.request.get_range("selected_day")
 
-        clients, venue_total_number, venue_total_cost = self.clients_table(chosen_year, chosen_month, chosen_day, venue_id)
+        clients, venue_total_number, venue_total_cost, venue_total_payment = self.clients_table(chosen_year, chosen_month, chosen_day, venue_id)
         chosen_venue = Venue.get_by_id(venue_id) if venue_id else None
         values = {
             'venues': Venue.query().fetch(),
             'clients': clients.values(),
             'venue_number': venue_total_number,
             'venue_expenditure': venue_total_cost,
+            'venue_payment': venue_total_payment,
             'chosen_venue': chosen_venue,
             'start_year': PROJECT_STARTING_YEAR,
             'end_year': datetime.now().year,
