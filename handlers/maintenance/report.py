da3e2e13ba224e6251ceb3ed4ab9eea742a016b8
dvpermyakov <dvpermyakov@edu.hse.ru>
2014-12-09 00:14:29 +0300

    fix style flaws

--- handlers/maintenance/report.py
+++ handlers/maintenance/report.py
@@ -7,43 +7,54 @@ import calendar
 
 PROJECT_STARTING_YEAR = 2014
 
+
 class ReportHandler(BaseHandler):
     def get(self):
         self.render('report.html')
 
 def suitable_date(day, month, year, beginning):
-    if not year: month = 0
-    if not month: day = 0
+    if not year:
+        month = 0
+    if not month:
+        day = 0
     if not beginning:
-        if not year: year = datetime.now().year
-        if not month: month = 12
-        if not day: day = calendar.monthrange(year,month)[1]
+        if not year:
+            year = datetime.now().year
+        if not month:
+            month = 12
+        if not day:
+            day = calendar.monthrange(year,month)[1]
         day = min(day, calendar.monthrange(year,month)[1])
         return datetime.combine(date(year, month, day), time.max)
     else:
-        if not year: year = PROJECT_STARTING_YEAR
-        if not month: month = 1
-        if not day: day = 1
+        if not year:
+            year = PROJECT_STARTING_YEAR
+        if not month:
+            month = 1
+        if not day:
+            day = 1
         day = min(day, calendar.monthrange(year,month)[1])
         return datetime.combine(date(year, month, day), time.min)
 
+
 class ReportedClient:
-    def __init__(self, id, name, tel, order_sum, payment):
-        self.id = id
+    def __init__(self, client_id, name, tel, order_sum, payment):
+        self.id = client_id
         self.name = name
         self.tel = tel
         self.amount_orders = 1
         self.total_sum = order_sum
         self.payment = payment
         self.average_order_cost = order_sum
-    def addOrder(self, order_sum, payment):
+
+    def add_order(self, order_sum, payment):
         self.amount_orders += 1
         self.total_sum += order_sum
         self.payment += payment
         self.average_order_cost = self.total_sum / self.amount_orders
 
-class ClientsReportHandler(BaseHandler):
 
+class ClientsReportHandler(BaseHandler):
     @staticmethod
     def clients_table(chosen_year=0, chosen_month=0, chosen_day=0, venue_id=0):
         clients = {}
@@ -57,24 +68,29 @@ class ClientsReportHandler(BaseHandler):
             total_sum = sum(item.get().price for item in order.items)
             payment = order.total_sum if order.payment_type_id != BONUS_PAYMENT_TYPE else 0
             if client_id in clients:
-                clients[client_id].addOrder(total_sum, payment)
+                clients[client_id].add_order(total_sum, payment)
             else:
                 client = Client.get_by_id(client_id)
                 clients[client_id] = ReportedClient(client_id, client.name, client.tel, total_sum, payment)
-        return clients,\
-               sum(client.amount_orders for client in clients.values()),\
-               sum(client.total_sum for client in clients.values()),\
-               sum(client.payment for client in clients.values())
+        return clients, \
+            sum(client.amount_orders for client in clients.values()), \
+            sum(client.total_sum for client in clients.values()), \
+            sum(client.payment for client in clients.values())
 
     def get(self):
-        # selected_*param == 0 if choose all *pararm
+        # selected_*param == 0 if choose all *param
         venue_id = self.request.get_range("selected_venue")
         chosen_year = self.request.get_range("selected_year")
         chosen_month = self.request.get_range("selected_month")
         chosen_day = self.request.get_range("selected_day")
-        if not chosen_year: chosen_month = 0
-        if not chosen_month: chosen_day = 0
-        clients, venue_total_number, venue_total_cost, venue_total_payment = self.clients_table(chosen_year, chosen_month, chosen_day, venue_id)
+        if not chosen_year:
+            chosen_month = 0
+        if not chosen_month:
+            chosen_day = 0
+        clients, venue_total_number, venue_total_cost, venue_total_payment = self.clients_table(chosen_year,
+                                                                                                chosen_month,
+                                                                                                chosen_day,
+                                                                                                venue_id)
         chosen_venue = Venue.get_by_id(venue_id) if venue_id else None
         values = {
             'venues': Venue.query().fetch(),
@@ -91,14 +107,17 @@ class ClientsReportHandler(BaseHandler):
         }
         self.render('reported_clients.html', **values)
 
+
 class ReportedMenuItem:
-     def __init__(self, id, title, price):
-         self.id = id
-         self.title = title
-         self.price = price
-         self.order_number = 1
-     def add_order(self):
-         self.order_number += 1
+    def __init__(self, item_id, title, price):
+        self.item_id = item_id
+        self.title = title
+        self.price = price
+        self.order_number = 1
+
+    def add_order(self):
+        self.order_number += 1
+
 
 class MenuItemsReportHandler(BaseHandler):
     @staticmethod
@@ -118,18 +137,21 @@ class MenuItemsReportHandler(BaseHandler):
                     item = MenuItem.get_by_id(item_id)
                     suited_menu_items[item_id] = ReportedMenuItem(item_id, item.title, item.price)
         return suited_menu_items, \
-               sum(item.order_number for item in suited_menu_items.values()),\
-               sum(item.order_number * item.price for item in suited_menu_items.values())
+            sum(item.order_number for item in suited_menu_items.values()),\
+            sum(item.order_number * item.price for item in suited_menu_items.values())
 
     def get(self):
-        # selected_*param == 0 if choose all *pararm
+        # selected_*param == 0 if choose all *param
         venue_id = self.request.get_range("selected_venue")
         chosen_year = self.request.get_range("selected_year")
         chosen_month = self.request.get_range("selected_month")
         chosen_day = self.request.get_range("selected_day")
         if not chosen_year: chosen_month = 0
         if not chosen_month: chosen_day = 0
-        menu_items, menu_item_total_number, menu_item_total_sum = self.menu_items_table(chosen_year, chosen_month, chosen_day, venue_id)
+        menu_items, menu_item_total_number, menu_item_total_sum = self.menu_items_table(chosen_year,
+                                                                                        chosen_month,
+                                                                                        chosen_day,
+                                                                                        venue_id)
         chosen_venue = Venue.get_by_id(venue_id) if venue_id else None
         values = {
             'venues': Venue.query().fetch(),
