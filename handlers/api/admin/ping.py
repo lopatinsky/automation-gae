31d0784842178faad2eb141288ec2f8d80f6be17
dvpermyakov <dvpermyakov@edu.hse.ru>
2014-12-15 19:06:20 +0300

    add fields in tablet_request item in db. change ping handler: 1) do not invoke error if geo 0 0 2) set additional fields in db 3) get location from status if geo 00

--- handlers/api/admin/ping.py
+++ handlers/api/admin/ping.py
@@ -17,6 +17,11 @@ class PingHandler(AdminApiHandler):
     def post(self):
         lat = float(self.request.get("lat"))
         lon = float(self.request.get("lon"))
+        error_number = self.request.get_range("error_number")
+        sound_level = self.request.get_range("sound_level")
+        is_in_charging = bool(self.request.get("is_in_charging"))
+        is_turned_on = bool(self.request.get("is_turned_on"))
+        app_version = self.request.get("app_version")
         geopt = GeoPt(lat, lon)
 
         status = AdminStatus.get(self.user.key.id(), self.token)
@@ -25,7 +30,7 @@ class PingHandler(AdminApiHandler):
             status.location = geopt
 
         distance = location.distance(geopt, status.location)
-        if distance > _MAX_DISTANCE_ALLOWED:
+        if _is_valid(geopt) and distance > _MAX_DISTANCE_ALLOWED:
             body = "Error: distance too large\n" \
                    "Initial coordinates: %s\n" \
                    "Current coordinates: %s\n" \
@@ -35,6 +40,10 @@ class PingHandler(AdminApiHandler):
             logging.error(body)
             email.send_error("ping", "Ping error", body)
         status.put()
-        history_item = TabletRequest(admin_id=status.admin.key.id(), token=self.token, location=status.location)
+        if not _is_valid(geopt):
+            geopt = status.location
+        history_item = TabletRequest(admin_id=status.admin.key.id(), token=self.token, location=geopt,
+                                     error_number=error_number, sound_level=sound_level, is_in_charging=is_in_charging,
+                                     is_turned_on=is_turned_on, app_version=app_version)
         history_item.put()
         self.render_json({})
--- models/tablet_request.py
+++ models/tablet_request.py
@@ -4,7 +4,12 @@ from google.appengine.ext import ndb
 
 
 class TabletRequest(ndb.Model):
-    admin_id = ndb.IntegerProperty(required=True)
-    token = ndb.StringProperty(required=True)
-    request_time = ndb.DateTimeProperty(auto_now_add=True)
+    admin_id = ndb.IntegerProperty(required=True, indexed=False)
+    token = ndb.StringProperty(required=True, indexed=False)
+    request_time = ndb.DateTimeProperty(auto_now_add=True, indexed=False)
     location = ndb.GeoPtProperty()
+    error_number = ndb.IntegerProperty(default=0, indexed=False)
+    sound_level = ndb.IntegerProperty(required=True, indexed=False)
+    is_in_charging = ndb.BooleanProperty(default=False, indexed=False)
+    is_turned_on = ndb.BooleanProperty(default=True, indexed=False)
+    app_version = ndb.StringProperty(required=True, indexed=False)
