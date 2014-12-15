7046047b657578432469af8b47d6247dd9e6eadf
dvpermyakov <dvpermyakov@edu.hse.ru>
2014-12-15 20:12:15 +0300

    add tablet request without additional params

--- handlers/api/admin/ping.py
+++ handlers/api/admin/ping.py
@@ -17,11 +17,19 @@ class PingHandler(AdminApiHandler):
     def post(self):
         lat = float(self.request.get("lat"))
         lon = float(self.request.get("lon"))
-        error_number = self.request.get_range("error_number")
-        sound_level = self.request.get_range("sound_level")
-        is_in_charging = bool(self.request.get("is_in_charging"))
-        is_turned_on = bool(self.request.get("is_turned_on"))
+        error_number = self.request.get("error_number")
+        error_number = None if not error_number else int(error_number)
+        sound_level_general = self.request.get("sound_level_general")
+        sound_level_general = None if not sound_level_general else int(sound_level_general)
+        sound_level_system = self.request.get("sound_level_system")
+        sound_level_system = None if not sound_level_system else int(sound_level_system)
+        is_in_charging = self.request.get("is_in_charging")
+        is_in_charging = None if not is_in_charging else bool(int(is_in_charging))
+        is_turned_on = self.request.get("is_turned_on")
+        is_turned_on = None if not is_turned_on else bool(int(is_turned_on))
         app_version = self.request.get("app_version")
+        if not app_version:
+            app_version = None
         geopt = GeoPt(lat, lon)
 
         status = AdminStatus.get(self.user.key.id(), self.token)
@@ -40,10 +48,9 @@ class PingHandler(AdminApiHandler):
             logging.error(body)
             email.send_error("ping", "Ping error", body)
         status.put()
-        if not _is_valid(geopt):
-            geopt = status.location
         history_item = TabletRequest(admin_id=status.admin.key.id(), token=self.token, location=geopt,
-                                     error_number=error_number, sound_level=sound_level, is_in_charging=is_in_charging,
+                                     error_number=error_number, sound_level_general=sound_level_general,
+                                     sound_level_system=sound_level_system, is_in_charging=is_in_charging,
                                      is_turned_on=is_turned_on, app_version=app_version)
         history_item.put()
         self.render_json({})
