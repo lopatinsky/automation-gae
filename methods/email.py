1f6f63412d76c80610903547a413be8d71f8c4ca
Mikhail Burshteyn <mdburshteyn@gmail.com>
2014-12-23 18:39:52 +0300

    Indicate test server in emalis

--- methods/email.py
+++ methods/email.py
@@ -6,6 +6,8 @@ _EMAIL_DOMAIN = "%s.appspotmail.com" % app_identity.get_application_id()
 
 def send_error(scope, subject, body):
     subject = "[DoubleB] " + subject
+    if config.DEBUG:
+        subject = "[Test]" + subject
     sender = "%s_errors@%s" % (scope, _EMAIL_DOMAIN)
     recipients = config.EMAILS.get(scope, "mdburshteyn@gmail.com")
     if recipients == "admins":
