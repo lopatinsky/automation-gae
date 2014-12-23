e4cb7f2c530f92450ab1e551c2a1acaee44ff2d6
dvpermyakov <dvpermyakov@edu.hse.ru>
2014-12-23 20:15:24 +0300

    update ping

--- handlers/api/admin/ping.py
+++ handlers/api/admin/ping.py
@@ -18,27 +18,26 @@ class PingHandler(AdminApiHandler):
         lat = float(self.request.get("lat"))
         lon = float(self.request.get("lon"))
 
-        error_number = self.request.get("error_number")
-        error_number = None if not error_number else int(error_number)
+        error_number = self.request.get_range("error_number")
 
-        sound_level_general = self.request.get("sound_level_general")
-        sound_level_general = None if not sound_level_general else int(sound_level_general)
+        sound_level_general = self.request.get_range("sound_level_general")
 
-        sound_level_system = self.request.get("sound_level_system")
-        sound_level_system = None if not sound_level_system else int(sound_level_system)
+        sound_level_system = self.request.get_range("sound_level_system")
 
         is_in_charging = self.request.get("is_in_charging")
-        is_in_charging = None if not is_in_charging else bool(int(is_in_charging))
+        if not is_in_charging is None:
+            is_in_charging = bool(is_in_charging)
 
         is_turned_on = self.request.get("is_turned_on")
-        is_turned_on = None if not is_turned_on else bool(int(is_turned_on))
+        if not is_turned_on is None:
+            is_turned_on = bool(is_turned_on)
 
-        battery_level = self.request.get("battery_level")
-        battery_level = None if not battery_level else int(battery_level)
+        battery_level = self.request.get_range("battery_level")
 
         app_version = self.request.get("app_version")
         if not app_version:
             app_version = None
+
         geopt = GeoPt(lat, lon)
 
         status = AdminStatus.get(self.user.key.id(), self.token)
