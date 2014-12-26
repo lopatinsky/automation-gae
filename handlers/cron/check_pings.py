3480e6d119d1f57bf7fee83a396dffc27914e140
dvpermyakov <dvpermyakov@edu.hse.ru>
2014-12-26 20:38:00 +0300

    update checkping

--- handlers/cron/check_pings.py
+++ handlers/cron/check_pings.py
@@ -2,23 +2,54 @@ import datetime
 import logging
 from webapp2 import RequestHandler
 from methods import email
-from models import AdminStatus
+from models import AdminStatus, TabletRequest, Admin
+
+
+AVAILABLE_BATTERY_LEVEL = 15
+AVAILABLE_SOUND_LEVEL = 0
 
 
 class CheckPingsHandler(RequestHandler):
     def get(self):
         now = datetime.datetime.utcnow()
-        delta = datetime.timedelta(minutes=10)
+        delta = datetime.timedelta(minutes=1)
         statuses = AdminStatus.query(AdminStatus.time < now - delta).fetch()
-        if not statuses:
-            return  # all pings up-to-date
-        status_template = "Token: %s\n" \
-                          "Last ping time: %s\n" \
-                          "Login: %s"
-        statuses_text = "\n\n".join(status_template % (status.key.id(), status.time, status.admin.email)
-                                    for status in statuses)
-        body = "Error: no ping in last 10 minutes\n" \
-               "Current server time: %s\n\n" \
-               "%s" % (now, statuses_text)
-        logging.error(body)
-        email.send_error("ping", "Ping error", body)
+
+        if statuses:
+            status_template = "Token: %s\n" \
+                              "Last ping time: %s\n" \
+                              "Login: %s"
+            statuses_text = "\n\n".join(status_template % (status.key.id(), status.time, status.admin.email)
+                                        for status in statuses)
+            body = "Error: no ping in last 10 minutes\n"\
+                   "Current server time: %s\n\n" \
+                   "%s" % (now, statuses_text)
+            logging.error(body)
+            email.send_error("ping", "Ping error", body)
+
+        admins = Admin.query().fetch()
+        for admin in admins:
+            info = ''
+            requests = TabletRequest.query(TabletRequest.request_time < now - delta,
+                                           TabletRequest.admin_id == admin.key.id()).fetch()
+            is_turned_on = False
+            is_low_sound = True
+            is_low_battery = True
+            for request in requests:
+                if request.is_turned_on:
+                    is_turned_on = True
+                if request.is_in_charging or request.battery_level > AVAILABLE_BATTERY_LEVEL:
+                    is_low_battery = False
+                if not request.sound_level_system > AVAILABLE_SOUND_LEVEL:
+                    is_low_sound = False
+            if not is_turned_on:
+                info += 'Tablet screen is turned off\n'
+            if is_low_battery and not info:
+                info += 'Tablet has low battery level\n'
+            if is_low_sound and not info:
+                info += 'Tablet has low sound level\n'
+
+            if info:
+                body = 'Id admin: %s\nErrors:\n%s' % (admin.email, info)
+                logging.error(body)
+                email.send_error("ping", "Ping error", body)
--- models/tablet_request.py
+++ models/tablet_request.py
@@ -4,7 +4,7 @@ from google.appengine.ext import ndb
 
 
 class TabletRequest(ndb.Model):
-    admin_id = ndb.IntegerProperty(required=True, indexed=False)
+    admin_id = ndb.IntegerProperty(required=True, indexed=True)
     token = ndb.StringProperty(required=True, indexed=False)
     request_time = ndb.DateTimeProperty(auto_now_add=True)
     location = ndb.GeoPtProperty(indexed=False)
