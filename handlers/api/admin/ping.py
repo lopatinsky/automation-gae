a3e5aac0ca9f801ecefa4d00f3c59d8d92293913
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-01-08 01:48:19 +0300

    Redo pings cron, remove emails from ping handler

--- handlers/api/admin/ping.py
+++ handlers/api/admin/ping.py
@@ -1,15 +1,7 @@
-import logging
 from google.appengine.ext.ndb import GeoPt
 from handlers.api.admin.base import AdminApiHandler
-from methods import location, email
 from methods.auth import api_user_required
 from models import AdminStatus, TabletRequest
-
-_MAX_DISTANCE_ALLOWED = 0.5
-
-
-def _is_valid(location):
-    return location.lat != 0 or location.lon != 0
 
 
 class PingHandler(AdminApiHandler):
@@ -19,9 +11,7 @@ class PingHandler(AdminApiHandler):
         lon = float(self.request.get("lon"))
 
         error_number = self.request.get_range("error_number")
-
         sound_level_general = self.request.get_range("sound_level_general")
-
         sound_level_system = self.request.get_range("sound_level_system")
 
         is_in_charging = self.request.get("is_in_charging", None)
@@ -33,29 +23,15 @@ class PingHandler(AdminApiHandler):
             is_turned_on = is_turned_on == "true"
 
         battery_level = self.request.get_range("battery_level")
-
-        app_version = self.request.get("app_version")
-        if not app_version:
-            app_version = None
+        app_version = self.request.get("app_version", None)
 
         geopt = GeoPt(lat, lon)
 
         status = AdminStatus.get(self.user.key.id(), self.token)
-        status_location_is_valid = _is_valid(status.location)
-        if not status_location_is_valid:  # GPS was disabled on startup, app sent zero coordinates
+        if status.location.lat == 0.0 and status.location.lon == 0.0:
             status.location = geopt
-
-        distance = location.distance(geopt, status.location)
-        if _is_valid(geopt) and distance > _MAX_DISTANCE_ALLOWED:
-            body = "Error: distance too large\n" \
-                   "Initial coordinates: %s\n" \
-                   "Current coordinates: %s\n" \
-                   "Distance: %s km\n" \
-                   "Login: %s\n"\
-                   "Token: %s" % (status.location, geopt, distance, self.user.email, status.key.id())
-            logging.error(body)
-            email.send_error("ping", "Ping error", body)
         status.put()
+
         history_item = TabletRequest(admin_id=status.admin.key.id(), token=self.token, location=geopt,
                                      error_number=error_number, sound_level_general=sound_level_general,
                                      sound_level_system=sound_level_system, is_in_charging=is_in_charging,
--- handlers/cron/check_pings.py
+++ handlers/cron/check_pings.py
@@ -1,58 +1,27 @@
-import datetime
 import logging
 from webapp2 import RequestHandler
 from methods import email
-from models import AdminStatus, TabletRequest, Admin
+from methods.pings import PingReport, LEVEL_OK
+from models import AdminStatus
 
 
-AVAILABLE_BATTERY_LEVEL = 15
-AVAILABLE_SOUND_LEVEL = 0
+_TEMPLATE = "Token: %s\n" \
+    "Login: %s\n" \
+    "%s"
 
 
 class CheckPingsHandler(RequestHandler):
     def get(self):
-        now = datetime.datetime.utcnow()
-        delta = datetime.timedelta(minutes=10)
-        statuses = AdminStatus.query(AdminStatus.time < now - delta).fetch()
+        statuses = AdminStatus.query().fetch()
 
-        body_error = ""
+        reports = [PingReport(status) for status in statuses]
+        error_reports = [report for report in reports if report.error_level > LEVEL_OK]
+        if not error_reports:
+            return
 
-        if statuses:
-            status_template = "Token: %s\n" \
-                              "Last ping time: %s\n" \
-                              "Login: %s"
-            statuses_text = "\n\n".join(status_template % (status.key.id(), status.time, status.admin.email)
-                                        for status in statuses)
-            body_error += "Error: no ping in last 10 minutes\n"\
-                          "Current server time: %s\n\n" \
-                          "%s" % (now, statuses_text)
-            logging.error(body_error)
+        error_messages = [_TEMPLATE % (report.admin_status.key.id(), report.admin_status.admin.email,
+                                       "\n".join(report.error_messages)) for report in error_reports]
+        body = "\n\n".join(error_messages)
 
-        admins = Admin.query().fetch()
-        for admin in admins:
-            info = ''
-            requests = TabletRequest.query(TabletRequest.request_time > now - delta,
-                                           TabletRequest.admin_id == admin.key.id()).fetch()
-            is_turned_on = False
-            is_low_sound = True
-            is_low_battery = True
-            for request in requests:
-                if request.is_turned_on:
-                    is_turned_on = True
-                if request.is_in_charging or request.battery_level > AVAILABLE_BATTERY_LEVEL:
-                    is_low_battery = False
-                if not request.sound_level_system > AVAILABLE_SOUND_LEVEL:
-                    is_low_sound = False
-            if not is_turned_on:
-                info += 'Tablet screen is turned off\n'
-            if is_low_battery and is_turned_on:
-                info += 'Tablet has low battery level\n'
-            if is_low_sound and is_turned_on:
-                info += 'Tablet has low sound level\n'
-
-            if info:
-                body_error += 'Id admin: %s\nErrors:\n%s\n\n' % (admin.email, info)
-                logging.error(body_error)
-
-        if body_error:
-            email.send_error("ping", "Ping error", body_error)
+        logging.info(body)
+        email.send_error("ping", "Tablet monitoring errors", body)
