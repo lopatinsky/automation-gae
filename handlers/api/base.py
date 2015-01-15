010ee5767017be3945f22a6a80c36ed0093edfef
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-01-15 17:47:18 +0300

    Replace 400 status code with 406 on iOS 7.0.4

--- handlers/api/base.py
+++ handlers/api/base.py
@@ -26,7 +26,16 @@ class ApiHandler(webapp2.RequestHandler):
             if key == "password":
                 value = "(VALUE HIDDEN)"
             logging.debug("%s: %s" % (key, value))
-        return super(ApiHandler, self).dispatch()
+
+        return_value = super(ApiHandler, self).dispatch()
+        if self.response.status_int == 400 and "iOS/7.0.4" in self.request.headers["User-Agent"]:
+            self.response.set_status(406)
+        return return_value
+
+    def abort(self, code, *args, **kwargs):
+        if code == 400 and "iOS/7.0.4" in self.request.headers["User-Agent"]:
+            code = 406
+        super(ApiHandler, self).abort(code, *args, **kwargs)
 
     def render_json(self, obj):
         self.response.headers["Content-Type"] = "application/json"
