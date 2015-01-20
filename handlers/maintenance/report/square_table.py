a9a461b0131f2c923484d998d3137995229731ba
dvpermyakov <dvpermyakov@edu.hse.ru>
2015-01-20 13:14:21 +0300

    add js code to select type

--- handlers/maintenance/report/square_table.py
+++ handlers/maintenance/report/square_table.py
@@ -8,13 +8,14 @@ WEEK = timedelta(days=7)
 
 
 class WeekInfo:
-    def __init__(self, goods_number, order_number, order_sum, gift_number, begin, end):
+    def __init__(self, goods_number, order_number, order_sum, gift_number, begin, end, cell_id):
         self.goods_number = goods_number
         self.order_number = order_number
         self.order_sum = order_sum
         self.gift_number = gift_number
         self.begin = begin
         self.end = end
+        self.cell_id = cell_id
 
 
 class SquareTableHandler(BaseHandler):
@@ -42,7 +43,6 @@ class SquareTableHandler(BaseHandler):
         return goods_number, order_number, order_sum, gift_number
 
     def get(self):
-        chosen_type = self.request.get("selected_type", 0)
 
         #orders = Order.query().fetch()
         clients = Client.query().fetch()
@@ -56,18 +56,21 @@ class SquareTableHandler(BaseHandler):
         start_time = clients[0].first_order_time
         current_time = start_time
         square = []
+        index_id = 0
         while current_time <= datetime.now():
             week_clients = self.get_clients_at_time(clients, current_time, current_time + WEEK)
             row_time = start_time
             row = []
             while row_time <= datetime.now():
+                index_id += 1
                 week_info_params = 0, 0, 0, 0
                 for client in week_clients:
                     week_orders = self.get_client_orders_at_time(client, row_time, row_time + WEEK)
                     week_info_params = tuple(sum(t) for t in zip(self.get_orders_info(week_orders), week_info_params))
                 goods_number, order_number, order_sum, gift_number = week_info_params
-                row.append(WeekInfo(goods_number, order_number, order_sum, gift_number, row_time, row_time + WEEK))
+                row.append(WeekInfo(goods_number, order_number, order_sum, gift_number, row_time, row_time + WEEK,
+                                    index_id))
                 row_time += WEEK
             square.append(row)
             current_time += WEEK
-        self.render('reported_square_table.html', square=square, chosen_type=chosen_type)
+        self.render('reported_square_table.html', square=square)
