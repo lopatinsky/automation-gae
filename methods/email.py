c2b674a89a34b49703fc4245e853c741293be5c1
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-01-21 17:20:41 +0300

    Fix html in emails

--- methods/email.py
+++ methods/email.py
@@ -4,16 +4,17 @@ from config import config
 _EMAIL_DOMAIN = "%s.appspotmail.com" % app_identity.get_application_id()
 
 
-def send_error(scope, subject, body, html=""):
+def send_error(scope, subject, body, html=None):
     subject = "[DoubleB] " + subject
     if config.DEBUG:
         subject = "[Test]" + subject
     sender = "%s_errors@%s" % (scope, _EMAIL_DOMAIN)
     recipients = config.EMAILS.get(scope, "mdburshteyn@gmail.com")
+    kw = {'html': html} if html else {}
     if recipients == "admins":
-        mail.send_mail_to_admins(sender, subject, body, html=html)
+        mail.send_mail_to_admins(sender, subject, body, **kw)
     else:
         try:
-            mail.send_mail(sender, recipients, subject, body, html=html)
+            mail.send_mail(sender, recipients, subject, body, **kw)
         except:
             pass
