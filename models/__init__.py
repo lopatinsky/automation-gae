0fb5f97e11954d867a17184a9f1018f85cdce44b
dvpermyakov <dvpermyakov@edu.hse.ru>
2015-01-26 17:12:59 +0300

    erase unneeded fields

--- models/__init__.py
+++ models/__init__.py
@@ -216,8 +216,6 @@ class Client(ndb.Model):
     created = ndb.DateTimeProperty(auto_now_add=True)
 
     name_confirmed = ndb.BooleanProperty(default=False)
-    last_push_date = ndb.DateTimeProperty(auto_now=True)
-    push_numbers = ndb.IntegerProperty(default=0)
 
     @classmethod
     def create(cls):
