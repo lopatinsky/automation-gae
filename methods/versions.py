b0cac7ed603cf741c248463ce4b9a4d12de6281a
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-03-04 23:21:18 +0300

    Add func to get version from request

---
+++ methods/versions.py
@@ -0,0 +1,19 @@
+import re
+from models import IOS_DEVICE, ANDROID_DEVICE
+
+
+def get_platform_and_version(request):
+    ua = request.user_agent
+    match = re.match('DoubleB/([0-9.]+)', ua)
+    if not match:
+        return None, 0
+
+    parts = match.group(1).split('.') + ['0', '0']
+    version = int(parts[0]) * 10000 + int(parts[1]) * 100 + int(parts[2])
+
+    if 'iOS' in ua:
+        return IOS_DEVICE, version
+    elif 'Android' in ua:
+        return ANDROID_DEVICE, version
+    else:
+        return None, version
