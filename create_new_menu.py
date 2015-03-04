ef490bb65812aced878e5b8f33db6696fa0bff42
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-03-04 23:31:53 +0300

    Minor changes in new menu

--- create_new_menu.py
+++ create_new_menu.py
@@ -1,11 +1,11 @@
 # coding=utf-8
 from google.appengine.ext import ndb
 
-from models import MenuCategory, MenuItem, STATUS_UNAVAILABLE
+from models import MenuCategory, MenuItem, STATUS_UNAVAILABLE, STATUS_AVAILABLE
 
 
 def do():
-    category = MenuCategory(id=2, title=u"Напитки", status=STATUS_UNAVAILABLE)
+    category = MenuCategory(id=2, title=u"Напитки", status=STATUS_AVAILABLE)
 
     start_id = 100
     items = []
@@ -14,9 +14,9 @@ def do():
         last_id = items[-1].key.id() if items else start_id
         return last_id + 1
 
-    def _add_item(title, price, beans=()):
+    def _add_item(title, price, beans=(), **kwargs):
         if not beans:
-            items.append(MenuItem(id=_next_id(), title=title, price=price))
+            items.append(MenuItem(id=_next_id(), title=title, price=price, **kwargs))
         else:
             for bean in beans:
                 _add_item("%s (%s)" % (title, bean), price)
@@ -28,7 +28,7 @@ def do():
     _add_item(u"Лунго", 150, std_beans)
     _add_item(u"Двойной эспрессо", 150)
 
-    _add_item(u"Айс-латте", 200)
+    _add_item(u"Айс-латте", 200, status=STATUS_UNAVAILABLE)
     _add_item(u"Капучино", 200)
 
     _add_item(u"Ванильный раф", 250)
@@ -38,12 +38,12 @@ def do():
     _add_item(u"Большой капучино", 250)
 
     _add_item(u"Лавандовый раф", 350)
-    _add_item(u"Латте крем-брюле", 350)
-    _add_item(u"Латте базилик", 350)
+    _add_item(u"Латте крем-брюле", 350, status=STATUS_UNAVAILABLE)
+    _add_item(u"Латте базилик", 350, status=STATUS_UNAVAILABLE)
     _add_item(u"Латте груша-бадьян", 350)
     _add_item(u"Латте шалфей", 350)
     _add_item(u"Клюквенный чай", 350)
-    _add_item(u"Чай огурец с ромашкой", 350)
+    _add_item(u"Чай огурец с ромашкой", 350, status=STATUS_UNAVAILABLE)
 
     category.menu_items = [item.key for item in items]
     ndb.put_multi([category] + items)
