3a0b9e397b093c416e41198de8d31a6ae45cc82b
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-03-08 11:35:13 +0300

    Fix imports

--- handlers/api/__init__.py
+++ handlers/api/__init__.py
@@ -14,5 +14,5 @@ from client import ClientHandler
 from history import HistoryHandler
 from response_order_status import CheckOrderSuccessHandler, ClientSettingSuccessHandler
 from update_order import UpdateOrderPromos
-from shared import GetSharedInfo, GetInvitationUrlHandler, GetGiftUrlHandler
+from shared import GetSharedInfo, GetInvitationUrlHandler, GetGiftUrlHandler, GetPreText
 from twilio import ReceiveSms
