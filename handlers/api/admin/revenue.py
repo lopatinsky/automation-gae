7c0b1c1459fb466592dfbb28a2e8756d6397e2d2
dvpermyakov <dvpermyakov1@gmail.com>
2015-04-24 13:04:51 +0300

    add revenue by days

--- handlers/api/admin/revenue.py
+++ handlers/api/admin/revenue.py
@@ -23,18 +23,30 @@ class RevenueReportTodayHandler(AdminApiHandler):
 
 
 class RevenueReportMonthHandler(AdminApiHandler):
-    @api_user_required
-    def get(self):
-        venue_key = self.user.venue
-        end = datetime.utcnow()
-        start = end.replace(hour=0, minute=0, day=1)
+    def get_orders(self, start, end, venue_key):
         if venue_key:
             orders = Order.query(Order.date_created > start, Order.date_created < end,
                                  Order.venue_id == venue_key.id()).fetch()
         else:
             orders = Order.query(Order.date_created > start, Order.date_created < end).fetch()
         orders = [order for order in orders if order.status == READY_ORDER]
-        self.render_json({
+        return {
+            'day': start.day,
             'orders': len(orders),
             'revenue': sum(order.total_sum for order in orders)
-        })
+        }
+
+    @api_user_required
+    def get(self):
+        venue_key = self.user.venue
+        end = datetime.utcnow()
+        start = end.replace(hour=0, minute=0, day=1)
+        results = []
+        while start < end:
+            if start.day < end.day:
+                start_end = start.replace(hour=0, minute=0, day=start.day+1)
+                results.append(self.get_orders(start, start_end, venue_key))
+            else:
+                results.append(self.get_orders(start, end, venue_key))
+            start += timedelta(days=1)
+        self.render_json(results)
