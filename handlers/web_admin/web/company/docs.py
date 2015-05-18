31703e6fa25a654d51e2c4839b19455120e2bfb8
dvpermyakov <dvpermyakov1@gmail.com>
2015-05-18 21:26:50 +0300

    add company info setting

--- handlers/web_admin/web/company/docs.py
+++ handlers/web_admin/web/company/docs.py
@@ -15,7 +15,11 @@ class AboutCompanyHandler(CompanyBaseHandler):
     @company_user_required
     def get(self):
         self.render('/docs/about.html', **{
-            'info': config.COMPANY_DESCRIPTION
+            'info': config.COMPANY_DESCRIPTION if config.COMPANY_DESCRIPTION else '',
+            'name': config.APP_NAME if config.APP_NAME else '',
+            'phone': config.SUPPORT_PHONE if config.SUPPORT_PHONE else '',
+            'site': config.SUPPORT_SITE if config.SUPPORT_SITE else '',
+            'emails': u''.join(config.SUPPORT_EMAILS)
         })
 
 
@@ -23,13 +27,25 @@ class SetAboutCompanyHandler(CompanyBaseHandler):
     @company_user_required
     def get(self):
         self.render('/docs/set_about.html', **{
-            'info': config.COMPANY_DESCRIPTION if config.COMPANY_DESCRIPTION else ''
+            'info': config.COMPANY_DESCRIPTION if config.COMPANY_DESCRIPTION else '',
+            'name': config.APP_NAME if config.APP_NAME else '',
+            'phone': config.SUPPORT_PHONE if config.SUPPORT_PHONE else '',
+            'site': config.SUPPORT_SITE if config.SUPPORT_SITE else '',
+            'emails': u''.join(config.SUPPORT_EMAILS)
         })
 
     @company_user_required
     def post(self):
         info = self.request.get('info')
+        name = self.request.get('name')
+        phone = self.request.get('phone')
+        site = self.request.get('site')
+        emails = self.request.get('emails').split(',')
         config = Config.get()
         config.COMPANY_DESCRIPTION = info
+        config.APP_NAME = name
+        config.SUPPORT_PHONE = phone
+        config.SUPPORT_SITE = site
+        config.SUPPORT_EMAILS = emails
         config.put()
         self.redirect('/company/docs/about')
