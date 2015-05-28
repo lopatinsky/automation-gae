13f21fab8f8074da63c3301ce901ee3ed8a5f0e4
dvpermyakov <dvpermyakov1@gmail.com>
2015-05-28 20:17:22 +0300

    add pay rules

--- handlers/api/docs.py
+++ handlers/api/docs.py
@@ -1,7 +1,23 @@
+# coding=utf-8
 from base import ApiHandler
 from config import config
 
 __author__ = 'dvpermyakov'
+
+
+def _get_values():
+    return {
+        'company_name': 'Meat Me Co',
+        'app_name': 'Meat Me',
+        'legal_person': u'ИП Иванов Иван Иванович',
+        'site': 'www.site.ru',
+        'legal_address': u'ул. Пушкина, дом. Колотушкина',
+        'legal_inn': 1241241241,
+        'legal_kpp': 412412412,
+        'legal_ogrn': 14124124124,
+        'legal_contacts': '+79152965155',
+        'legal_support_email': 'dvpermyakov1@gmail.com'
+    }
 
 
 class AboutHandler(ApiHandler):
@@ -13,14 +29,14 @@ class AboutHandler(ApiHandler):
 
 class LicenceHandler(ApiHandler):
     def get(self):
-        self.render_doc('licence_agreement.html')
+        self.render_doc('auto_licence_agreement.html', **_get_values())
 
 
 class NdaHandler(ApiHandler):
     def get(self):
-        self.render_doc('licence_agreement.html')
+        self.render_doc('nda.html')
 
 
 class PaymentRulesHandler(ApiHandler):
     def get(self):
-        self.render_doc('payment_rules.html')
+        self.render_doc('auto_payment_rules.html', **_get_values())
