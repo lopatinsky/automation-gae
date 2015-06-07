7ae93408cdb9e8ce6e47b71a34c6744de6079603
dvpermyakov <dvpermyakov1@gmail.com>
2015-06-07 21:36:49 +0300

    minor changes in delivery types web

--- handlers/web_admin/web/company/delivery_types.py
+++ handlers/web_admin/web/company/delivery_types.py
@@ -20,15 +20,6 @@ class DeliveryTypesHandler(CompanyBaseHandler):
                 'max_time': delivery.max_time
             }
 
-        if not DeliverySlot.query().fetch():
-            DeliverySlot(name=u'Сейчас', slot_type=0, value=0).put()
-            DeliverySlot(name=u'Через 5 минут', slot_type=0, value=5).put()
-            DeliverySlot(name=u'Через 10 минут', slot_type=0, value=10).put()
-            DeliverySlot(name=u'Через 15 минут', slot_type=0, value=15).put()
-            DeliverySlot(name=u'Через 20 минут', slot_type=0, value=20).put()
-            DeliverySlot(name=u'Через 25 минут', slot_type=0, value=25).put()
-            DeliverySlot(name=u'Через 30 минут', slot_type=0, value=30).put()
-
         venues = Venue.query().fetch()
         for venue in venues:
             deliveries = {}
@@ -71,6 +62,15 @@ class DeliveryTypesHandler(CompanyBaseHandler):
 class DeliverySlotListHandler(CompanyBaseHandler):
     def get(self):
         slots = DeliverySlot.query().order(DeliverySlot.value).fetch()
+        if not slots:
+            DeliverySlot(name=u'Сейчас', slot_type=0, value=0).put()
+            DeliverySlot(name=u'Через 5 минут', slot_type=0, value=5).put()
+            DeliverySlot(name=u'Через 10 минут', slot_type=0, value=10).put()
+            DeliverySlot(name=u'Через 15 минут', slot_type=0, value=15).put()
+            DeliverySlot(name=u'Через 20 минут', slot_type=0, value=20).put()
+            DeliverySlot(name=u'Через 25 минут', slot_type=0, value=25).put()
+            DeliverySlot(name=u'Через 30 минут', slot_type=0, value=30).put()
+            slots = DeliverySlot.query().order(DeliverySlot.value).fetch()
         for slot in slots:
             slot.slot_type_str = DeliverySlot.CHOICES_MAP[slot.slot_type]
         self.render('/delivery_settings/delivery_slot_list.html', slots=slots)
@@ -108,7 +108,7 @@ class ChooseSlotsHandler(CompanyBaseHandler):
         self.render('/delivery_settings/choose_delivery_slots.html', **{
             'venue': venue,
             'delivery_type': delivery_type,
-            'slots': DeliverySlot.query().fetch()
+            'slots': DeliverySlot.query().order(DeliverySlot.value).fetch()
         })
 
     def post(self):
