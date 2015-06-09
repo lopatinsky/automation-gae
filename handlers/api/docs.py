a24f8ef6a7622b3d7652f6e913c459d615dd0d36
dvpermyakov <dvpermyakov1@gmail.com>
2015-06-09 19:06:56 +0300

    fix ip and ooo logic

--- handlers/api/docs.py
+++ handlers/api/docs.py
@@ -9,12 +9,14 @@ def _get_values():
     return {
         'company_name': config.COMPANY_NAME,
         'app_name': config.APP_NAME,
-        'legal_person': config.LEGAL_PERSON,
+        'legal_person': config.LEGAL_PERSON if config.LEGAL_PERSON else config.LEGAL_PERSON_IP,
+        'legal_person_ip': config.LEGAL_PERSON_IP,
         'site': config.SUPPORT_SITE,
         'legal_address': config.COMPANY_ADDRESS,
         'legal_inn': config.INN,
         'legal_kpp': config.KPP,
         'legal_ogrn': config.OGRN,
+        'legal_ogrnip': config.OGRNIP,
         'legal_contacts': config.LEGAL_CONTACTS,
         'legal_support_email': config.LEGAL_EMAIL
     }
