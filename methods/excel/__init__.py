e4b6ee3056cdcb4ca313a2d990467ff8abc3d7d2
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-06-28 01:23:33 +0300

    Floats in excel reports

--- methods/excel/__init__.py
+++ methods/excel/__init__.py
@@ -16,8 +16,10 @@ def send_excel_file(request_handler, name, template_name, **values):
         for j, td in enumerate(tr.getchildren()):
             if td.text:
                 td = u''.join(td.text)
-                if td.isdigit():
-                    td = int(td)
+                try:
+                    td = float(td)
+                except ValueError:
+                    pass
                 sheet.write(i, j, td)
 
     request_handler.response.headers['Content-Type'] = 'application/ms-excel'
