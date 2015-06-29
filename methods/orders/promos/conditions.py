4c6412c34f59c84af6e856c80cc1e331a26f0b7d
dvpermyakov <dvpermyakov1@gmail.com>
2015-06-29 12:30:23 +0300

    add index for check first order, why not + more logs

--- methods/orders/promos/conditions.py
+++ methods/orders/promos/conditions.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 import logging
+from google.appengine.api.namespace_manager import namespace_manager
 from models import Order
 from models.order import NOT_CANCELED_STATUSES
 
@@ -17,6 +18,11 @@ def check_condition_min_by_value(condition, value):
 
 
 def check_first_order(client):
+    logging.info('client_id = %s' % client.key.id())
+    logging.info('statuses = %s' % NOT_CANCELED_STATUSES)
+    logging.info('namespace = %s' % namespace_manager.get_namespace())
+    logging.info('all client orders = %s' % Order.query().fetch())
+    logging.info('order by fetch, not get, orders = %s' % Order.query(Order.client_id == client.key.id(), Order.status.IN(NOT_CANCELED_STATUSES)).get())
     order = Order.query(Order.client_id == client.key.id(), Order.status.IN(NOT_CANCELED_STATUSES)).get()
     logging.info('check first order, order = %s' % order)
     return order is None
