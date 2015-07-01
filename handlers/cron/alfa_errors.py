1ae64286113e8ec9352d0733712b458601415a28
dvpermyakov <dvpermyakov1@gmail.com>
2015-07-01 15:52:55 +0300

    rewrite creating order cron + alfa errors cron

--- handlers/cron/alfa_errors.py
+++ handlers/cron/alfa_errors.py
@@ -1,33 +1,44 @@
 from collections import defaultdict, Counter
 import datetime
 import logging
+from google.appengine.api.namespace_manager import namespace_manager
+from google.appengine.ext.ndb import metadata
 from webapp2 import RequestHandler
 from methods import email
 from models.error_statistics import PaymentErrorsStatistics
 
 
+MINUTES_INTERVAL = 10
+AVAIL_FAILURE_PERCENT = 20
+
+
 class CheckAlfaErrorsHandler(RequestHandler):
     def get(self):
         now = datetime.datetime.now()
-        since = now - datetime.timedelta(minutes=10)
-        requests = PaymentErrorsStatistics.get_requests(since)
-        if not requests:
-            return
-        failure_count = sum(int(not r.success) for r in requests)
-        if float(failure_count) / len(requests) > 0.2:
-            failure_info = defaultdict(Counter)  # url -> {message -> count}
-            for request in requests:
-                if not request.success:
-                    failure_info[request.url][request.error_message] += 1
-
-            error_details = []
-            for url, messages in failure_info.iteritems():
-                messages_string = "\n".join("| %s: %s" % item for item in messages.items())
-                error_details.append("%s\n%s" % (url, messages_string))
-
-            body = "Last 10 minutes: %s errors out of %s requests\n\n%s" % \
-                   (failure_count, len(requests), "\n\n".join(error_details))
-
-            logging.warning(body)
-
-            email.send_error("server", "Payment errors", body)
+        since = now - datetime.timedelta(minutes=MINUTES_INTERVAL)
+        namespace_errors = {}
+        for namespace in metadata.get_namespaces():
+            namespace_manager.set_namespace(namespace)
+            requests = PaymentErrorsStatistics.get_requests(since)
+            if requests:
+                logging.info('-----------------------------')
+                failure_count = sum(int(not r.success) for r in requests)
+                if float(failure_count) / len(requests) > AVAIL_FAILURE_PERCENT / 100.0:
+                    failure_info = defaultdict(Counter)  # url -> {message -> count}
+                    for request in requests:
+                        if not request.success:
+                            failure_info[request.url][request.error_message] += 1
+                    error_details = []
+                    for url, messages in failure_info.iteritems():
+                        messages_string = "\n".join("| %s: %s" % item for item in messages.items())
+                        error_details.append("%s\n%s" % (url, messages_string))
+                    text = u"%s errors out of %s requests\n\n%s" % (failure_count, len(requests), "\n\n".join(error_details))
+                    namespace_errors[namespace] = text
+        mail_body = u'Errors with AlfaBank within %s minutes and with %s%% of failures:\n' % \
+                    (MINUTES_INTERVAL, AVAIL_FAILURE_PERCENT)
+        for namespace in namespace_errors.keys():
+            mail_body += u'In namespace = %s:\n' % namespace
+            mail_body += namespace_errors[namespace]
+        if namespace_errors:
+            logging.warning(mail_body)
+            email.send_error("server", "Payment errors", mail_body)
--- handlers/cron/creating_orders.py
+++ handlers/cron/creating_orders.py
@@ -1,59 +1,64 @@
 import datetime
-import webapp2
+from google.appengine.api.namespace_manager import namespace_manager
+from google.appengine.ext.ndb import metadata
+from webapp2 import RequestHandler
 from methods import email, alfa_bank
 from models import Order
 from models.order import CREATING_ORDER
 
+MINUTES_INTERVAL = 10
 
-class CheckCreatingOrdersHandler(webapp2.RequestHandler):
+
+class CheckCreatingOrdersHandler(RequestHandler):
     def get(self):
         now = datetime.datetime.now()
-        delta = datetime.timedelta(minutes=2)
-        orders = Order.query(Order.status == CREATING_ORDER, Order.date_created <= now - delta).fetch()
-        if not orders:
-            return
+        delta = datetime.timedelta(minutes=MINUTES_INTERVAL)
+        namespace_infos = {}
+        for namespace in metadata.get_namespaces():
+            namespace_manager.set_namespace(namespace)
+            orders = Order.query(Order.status == CREATING_ORDER, Order.date_created <= now - delta).fetch()
+            if orders:
+                infos = []
+                for order in orders:
+                    info = [
+                        ("id", order.key.id()),
+                        ("payment type", order.payment_type_id),
+                        ("payment id", order.payment_id)
+                    ]
+                    to_delete = False
+                    if order.has_card_payment:
+                        try:
+                            # check payment status
+                            status = alfa_bank.check_extended_status(order.payment_id)["alfa_response"]
+                            info.append(("status check result", status))
 
-        infos = []
-
-        for order in orders:
-            info = [
-                ("id", order.key.id()),
-                ("payment type", order.payment_type_id),
-                ("payment id", order.payment_id)
-            ]
-            to_delete = False
-            if order.has_card_payment:
-                try:
-                    # check payment status
-                    status = alfa_bank.check_extended_status(order.payment_id)["alfa_response"]
-                    info.append(("status check result", status))
-
-                    # if status check was successful:
-                    if str(status.get("errorCode", '0')) == '0':
-                        # money already deposited -- do not delete
-                        if status['orderStatus'] == 2:
-                            info.append(("ERROR", "deposited"))
-                        # money approved -- reverse
-                        elif status['orderStatus'] == 1:
-                            reverse_result = alfa_bank.reverse(order.payment_id)
-                            info.append(("reverse result", reverse_result))
-                            if str(reverse_result.get('errorCode', '0')) == '0':
-                                to_delete = True
-                        # any other status is OK to delete
-                        else:
-                            to_delete = True
-                except Exception as e:
-                    info.append(("exception", repr(e)))
-            else:
-                to_delete = True
-            if to_delete:
-                order.key.delete()
-                info.append(("deleted", True))
-            infos.append(info)
-
-        body = "List of orders:\n\n" + \
-               "\n\n".join(
-                   "\n".join("%s: %s" % t for t in info)
-                   for info in infos
-               )
-        email.send_error("order", "Orders crashed while creating", body)
+                            # if status check was successful:
+                            if str(status.get("errorCode", '0')) == '0':
+                                # money already deposited -- do not delete
+                                if status['orderStatus'] == 2:
+                                    info.append(("ERROR", "deposited"))
+                                # money approved -- reverse
+                                elif status['orderStatus'] == 1:
+                                    reverse_result = alfa_bank.reverse(order.payment_id)
+                                    info.append(("reverse result", reverse_result))
+                                    if str(reverse_result.get('errorCode', '0')) == '0':
+                                        to_delete = True
+                                # any other status is OK to delete
+                                else:
+                                    to_delete = True
+                        except Exception as e:
+                            info.append(("exception", repr(e)))
+                    else:
+                        to_delete = True
+                    if to_delete:
+                        order.key.delete()
+                        info.append(("deleted", True))
+                    infos.append(info)
+                namespace_infos[namespace] = infos
+        mail_body = "Orders with creating status"
+        for namespace in namespace_infos.keys():
+            mail_body += 'In namespace = %s:\n' % namespace
+            mail_body += "List of orders:\n" + \
+                         "\n\n".join("\n".join("%s: %s" % t for t in info) for info in namespace_infos[namespace])
+        if namespace_infos:
+            email.send_error("order", "Orders crashed while creating", mail_body)
