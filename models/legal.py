dd87e2f5193992ff0529b49cfdf514b27616cd8c
dvpermyakov <dvpermyakov@edu.hse.ru>
2015-07-08 01:30:25 +0600

    render multiple legals

--- models/legal.py
+++ models/legal.py
@@ -1,9 +1,11 @@
 from google.appengine.ext import ndb
+from models import STATUS_AVAILABLE, STATUS_CHOICES
 
 __author__ = 'dvpermyakov'
 
 
 class LegalInfo(ndb.Model):
+    status = ndb.IntegerProperty(choices=STATUS_CHOICES, default=STATUS_AVAILABLE)
     name = ndb.StringProperty(required=True)
     address = ndb.StringProperty(required=True)
     site = ndb.StringProperty(required=True)
