81e8b4c3f96033f8cf034ba4f7d6906c5421310c
dvpermyakov <dvpermyakov1@gmail.com>
2015-09-01 16:13:22 +0300

    add wallet enabled in company info

--- handlers/api/company.py
+++ handlers/api/company.py
@@ -49,6 +49,9 @@ class CompanyInfoHandler(ApiHandler):
             'colors': {
                 'action': config.ACTION_COLOR,
             },
+            'wallet': {
+                'enable': config.WALLET_ENABLED,
+            },
             'share_gift': {
                 'enabled': config.SHARED_GIFT_ENABLED,
             },
