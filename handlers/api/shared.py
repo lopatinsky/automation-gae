ae23b5063e0e3fe1dea049149fa77b92c4e206db
dvpermyakov <dvpermyakov1@gmail.com>
2015-09-29 14:25:00 +0300

    429 on shared/invitation/get_url

--- handlers/api/shared.py
+++ handlers/api/shared.py
@@ -47,6 +47,8 @@ class GetInvitationUrlHandler(ApiHandler):
             self.abort(403)
         share = Share.query(Share.sender == client.key, Share.status == Share.ACTIVE,
                             Share.share_type == branch_io.INVITATION).get()
+        if not share.promo_code and not share.channel_urls:
+            self.abort(429)
         logging.info('Found share: %s' % share)
         if not share:
             share = Share(share_type=branch_io.INVITATION, sender=client.key)
