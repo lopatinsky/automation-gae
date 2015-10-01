cb67b2de3908e28253b31bf7e21517d962715b33
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-10-01 18:22:41 +0300

    Fix default namespace

--- appengine_config.py
+++ appengine_config.py
@@ -1,9 +1,8 @@
 import os
-from google.appengine.api.namespace_manager import namespace_manager
 
 
 def namespace_manager_default_namespace_for_request():
     if len(os.environ['SERVER_NAME'].split('.')) < 5:
-        return namespace_manager.google_apps_namespace()
+        return ''
     else:
         return os.environ['SERVER_NAME'].split('.')[0]
