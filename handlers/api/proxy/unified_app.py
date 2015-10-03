acdf7d31cb5d3c481f8cb9f37f7308a2b741d217
dvpermyakov <dvpermyakov1@gmail.com>
2015-10-03 17:41:51 +0300

    extend company dict

--- handlers/api/proxy/unified_app.py
+++ handlers/api/proxy/unified_app.py
@@ -1,22 +1,11 @@
-from google.appengine.api.namespace_manager import namespace_manager
-
 from ..base import ApiHandler
-from models.config.config import Config
 from models import STATUS_AVAILABLE
 from models.proxy.unified_app import AutomationCompany
 
 
 class CompaniesHandler(ApiHandler):
     def get(self):
-        companies = AutomationCompany.query(AutomationCompany.status == STATUS_AVAILABLE).fetch()
-        company_dicts = []
-        for company in companies:
-            namespace_manager.set_namespace(company.namespace)
-            config = Config.get()
-            company_dicts.append({
-                'namespace': company.namespace,
-                'name': config.APP_NAME
-            })
         self.render_json({
-            'companies': company_dicts
+            'companies': [company.dict()
+                          for company in AutomationCompany.query(AutomationCompany.status == STATUS_AVAILABLE).fetch()]
         })
