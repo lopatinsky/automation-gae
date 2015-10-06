2c81206f5e235008c37a13566f75e086491168f9
dvpermyakov <dvpermyakov1@gmail.com>
2015-10-06 18:08:03 +0300

    update config versions + unable versions

--- methods/versions.py
+++ methods/versions.py
@@ -27,28 +27,29 @@ def is_test_version(url):
     return test
 
 
-def get_version(version):
+def get_version(version, create=False):
     version = int(version)
+    version_obj = None
     config = Config.get()
     for version_obj in config.VERSIONS:
         if version_obj.number == version:
             return version_obj
-    now = datetime.utcnow()
-    version_obj = Version(created=now, updated=now, number=version)
-    config.VERSIONS.append(version_obj)
-    config.put()
+    if create:
+        now = datetime.utcnow()
+        version_obj = Version(created=now, updated=now, number=version)
+        config.VERSIONS.append(version_obj)
+        config.put()
     return version_obj
 
 
 def is_available_version(version):
     version = int(version)
     version_obj = get_version(version)
-    return version_obj.available
+    return version_obj and version_obj.available
 
 
 def update_company_versions(version):
     version = int(version)
-    config = Config.get()
-    version_obj = get_version(version)
+    version_obj = get_version(version, create=True)
     version_obj.updated = datetime.utcnow()
-    config.put()
+    version_obj.put()
