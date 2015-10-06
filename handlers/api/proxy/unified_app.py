cab90e30373499b613c9c748c5c40563455f1e3e
dvpermyakov <dvpermyakov1@gmail.com>
2015-10-06 17:05:02 +0300

    use init namespace in proxy handlers

--- handlers/api/proxy/unified_app.py
+++ handlers/api/proxy/unified_app.py
@@ -1,3 +1,4 @@
+from google.appengine.api.namespace_manager import namespace_manager
 from ..base import ApiHandler
 from models import STATUS_AVAILABLE
 from models.proxy.unified_app import AutomationCompany
@@ -5,6 +6,8 @@ from models.proxy.unified_app import AutomationCompany
 
 class CompaniesHandler(ApiHandler):
     def get(self):
+        if self.request.init_namespace:
+            namespace_manager.set_namespace(self.request.init_namespace)
         self.render_json({
             'companies': [company.dict()
                           for company in AutomationCompany.query(AutomationCompany.status == STATUS_AVAILABLE).fetch()]
