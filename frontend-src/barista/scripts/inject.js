3c9d5651a2b7341a5c925edcc97b5f42d4813eb7
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-10-07 21:03:37 +0300

    Add webfont late to improve loading speed

--- frontend-src/barista/scripts/inject.js
+++ frontend-src/barista/scripts/inject.js
@@ -1,3 +1,8 @@
 import injectTapEventPlugin from "react-tap-event-plugin";
 import _ from 'core-js';
 injectTapEventPlugin();
+
+const fontStyleLink = document.createElement("link");
+fontStyleLink.rel = "stylesheet";
+fontStyleLink.href = "http://fonts.googleapis.com/css?family=Roboto:300,400,500,700&subset=latin,cyrillic";
+document.head.appendChild(fontStyleLink);
