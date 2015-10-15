838d18d234a2a9a905ae5a7ab4d94a06b10d2b38
dvpermyakov <dvpermyakov1@gmail.com>
2015-10-15 21:27:44 +0300

    update client info dialog

--- frontend-src/app/scripts/components/ClientInfoDialog.jsx
+++ frontend-src/app/scripts/components/ClientInfoDialog.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dialog, TextField, FlatButton } from 'material-ui';
+import { Dialog, TextField, FlatButton, Card } from 'material-ui';
 import { ClientStore } from '../stores';
 import Actions from '../Actions';
 
@@ -41,10 +41,36 @@ const ClientInfoDialog = React.createClass({
     render() {
         return (
             <Dialog ref="clientInfoDialog">
-                <TextField hintText="Имя" ref="name" value={this.state.name} onChange={this._refresh}/>
-                <TextField hintText="Номер телефона" ref="phone" value={this.state.phone} onChange={this._refresh}/>
-                <TextField hintText="Email" ref="email" value={this.state.email} onChange={this._refresh}/>
-                <FlatButton label="Ок" onClick={this._submit} />
+                <Card style={{margin: '0 0 12px 0'}}>
+                    <TextField
+                        style={{margin: '0 0 0 12px'}}
+                        hintText="Имя"
+                        floatingLabelText="Имя"
+                        ref="name"
+                        value={this.state.name}
+                        onChange={this._refresh}/>
+                </Card>
+                <Card style={{margin: '0 0 12px 0'}}>
+                    <TextField
+                        style={{margin: '0 0 0 12px'}}
+                        hintText="Номер телефона"
+                        floatingLabelText="Номер телефона"
+                        ref="phone"
+                        value={this.state.phone}
+                        onChange={this._refresh}/>
+                </Card>
+                <Card style={{margin: '0 0 12px 0'}}>
+                    <TextField
+                        style={{margin: '0 0 0 12px'}}
+                        hintText="Email"
+                        floatingLabelText="Email"
+                        ref="email"
+                        value={this.state.email}
+                        onChange={this._refresh}/>
+                </Card>
+                <div>
+                    <FlatButton label="Ок" onClick={this._submit} />
+                </div>
             </Dialog>
         );
     }
