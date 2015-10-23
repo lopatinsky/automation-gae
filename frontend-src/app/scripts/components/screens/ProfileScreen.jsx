a362c9ac5873a1391fe5e7d09771d23dfd0678cf
dvpermyakov <dvpermyakov1@gmail.com>
2015-10-23 18:17:17 +0300

    update profile screen re #298

--- frontend-src/app/scripts/components/screens/ProfileScreen.jsx
+++ frontend-src/app/scripts/components/screens/ProfileScreen.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { TextField, Card, FlatButton } from 'material-ui';
+import { TextField, Card, FlatButton, FontIcon } from 'material-ui';
 import { ClientStore } from '../../stores';
 import { ServerRequests } from '../../actions';
 import { AppActions } from '../../actions';
+import settings from '../../settings';
 
 const ProfileScreen = React.createClass({
      _refresh() {
@@ -28,33 +29,66 @@ const ProfileScreen = React.createClass({
 
     render() {
         return <div style={{padding: '76px 0 0 0'}}>
-            <Card style={{margin: '0 12px 12px 12px'}}>
-                <TextField
-                    style={{margin: '0 0 0 12px', width: '95%'}}
-                    hintText="Имя"
-                    floatingLabelText="Имя"
-                    ref="name"
-                    value={this.state.name}
-                    onChange={this._refresh}/>
-            </Card>
-            <Card style={{margin: '0 12px 12px 12px'}}>
-                <TextField
-                    style={{margin: '0 0 0 12px', width: '95%'}}
-                    hintText="Номер телефона"
-                    floatingLabelText="Номер телефона"
-                    ref="phone"
-                    value={this.state.phone}
-                    onChange={this._refresh}/>
-            </Card>
-            <Card style={{margin: '0 12px 12px 12px'}}>
-                <TextField
-                    style={{margin: '0 0 0 12px', width: '95%'}}
-                    hintText="Email"
-                    floatingLabelText="Email"
-                    ref="email"
-                    value={this.state.email}
-                    onChange={this._refresh}/>
-            </Card>
+            <div style={{width: '100%', display: 'table'}}>
+                <Card style={{margin: '0 12px 12px 12px'}}>
+                    <div style={{display: 'table-cell', padding: '0 9px 0 9px'}}>
+                        <FontIcon style={{verticalAlign: 'middle', fontSize: '18px'}}
+                                  color={settings.primaryColor}
+                                  className="material-icons">
+                            face
+                        </FontIcon>
+                    </div>
+                    <div style={{display: 'table-cell', width: '95%'}}>
+                        <TextField
+                            style={{width: '100%'}}
+                            hintText="Имя"
+                            floatingLabelText="Имя"
+                            ref="name"
+                            value={this.state.name}
+                            onChange={this._refresh}/>
+                    </div>
+                </Card>
+            </div>
+            <div style={{width: '100%', display: 'table'}}>
+                <Card style={{margin: '0 12px 12px 12px'}}>
+                    <div style={{display: 'table-cell', padding: '0 9px 0 9px'}}>
+                        <FontIcon style={{verticalAlign: 'middle', fontSize: '18px'}}
+                                  color={settings.primaryColor}
+                                  className="material-icons">
+                            stay_primary_portrait
+                        </FontIcon>
+                    </div>
+                    <div style={{display: 'table-cell', width: '95%'}}>
+                        <TextField
+                            style={{width: '100%'}}
+                            hintText="Номер телефона"
+                            floatingLabelText="Номер телефона"
+                            ref="phone"
+                            value={this.state.phone}
+                            onChange={this._refresh}/>
+                    </div>
+                </Card>
+            </div>
+            <div style={{width: '100%', display: 'table'}}>
+                <Card style={{margin: '0 12px 12px 12px'}}>
+                    <div style={{display: 'table-cell', padding: '0 9px 0 9px'}}>
+                        <FontIcon style={{verticalAlign: 'middle', fontSize: '18px'}}
+                                  color={settings.primaryColor}
+                                  className="material-icons">
+                            mail
+                        </FontIcon>
+                    </div>
+                    <div style={{display: 'table-cell', width: '95%'}}>
+                        <TextField
+                            style={{width: '100%'}}
+                            hintText="Email"
+                            floatingLabelText="Email"
+                            ref="email"
+                            value={this.state.email}
+                            onChange={this._refresh}/>
+                    </div>
+                </Card>
+            </div>
         </div>;
     }
 });
