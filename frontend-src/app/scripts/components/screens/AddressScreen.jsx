6f0a5964be7948031b60dbbb996fb699511976e4
dvpermyakov <dvpermyakov1@gmail.com>
2015-10-29 17:06:59 +0300

    update address screen re #298

--- frontend-src/app/scripts/components/screens/AddressScreen.jsx
+++ frontend-src/app/scripts/components/screens/AddressScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { DropDownMenu, TextField, Paper, Card } from 'material-ui';
+import { DropDownMenu, TextField, Paper, Card, FontIcon } from 'material-ui';
 import { AddressStore } from '../../stores';
+import settings from '../../settings';
 
 const AddressScreen = React.createClass({
      _refresh() {
@@ -44,47 +45,83 @@ const AddressScreen = React.createClass({
             }
         );
         return <div style={{padding: '76px 0 0 0'}}>
-            <div style={{width: '100%'}}>
+            <div style={{width: '100%', display: 'table'}}>
                 <Paper style={{margin: '0 12px 0 12px', zIndex: '9'}}>
-                    <DropDownMenu
-                        style={{zIndex: '10', width: '100%'}}
-                        underlineStyle={{display: 'none'}}
-                        menuItems={cities}
-                        selectedIndex={AddressStore.getCityIndex(AddressStore.getChosenCity())}
-                        onChange={this._onCityTap}/>
+                    <div style={{display: 'table-cell', verticalAlign: 'middle', padding: '0 9px 0 9px'}}>
+                        <FontIcon style={{verticalAlign: 'middle', fontSize: '20px'}}
+                                  color={settings.primaryColor}
+                                  className="material-icons">
+                            location_city
+                        </FontIcon>
+                    </div>
+                    <div style={{display: 'table-cell', width: '95%'}}>
+                        <DropDownMenu
+                            style={{zIndex: '10', width: '100%'}}
+                            underlineStyle={{display: 'none'}}
+                            menuItems={cities}
+                            selectedIndex={AddressStore.getCityIndex(AddressStore.getChosenCity())}
+                            onChange={this._onCityTap}/>
+                    </div>
                 </Paper>
             </div>
-            <div style={{width: '100%'}}>
+            <div style={{width: '100%', display: 'table'}}>
                 <Card style={{margin: '12px 12px 0 12px'}}>
-                    <TextField
-                        style={{margin: '0 12px 6px 12px', width: '95%'}}
-                        hintText="Улица"
-                        floatingLabelText="Улица"
-                        ref="street"
-                        value={this.state.street}
-                        onChange={this._refresh}/>
+                    <div style={{display: 'table-cell', padding: '0 9px 0 9px'}}>
+                        <FontIcon style={{verticalAlign: 'middle', fontSize: '20px'}}
+                                  color={settings.primaryColor}
+                                  className="material-icons">
+                            traffic
+                        </FontIcon>
+                    </div>
+                    <div style={{display: 'table-cell', width: '95%'}}>
+                        <TextField
+                            style={{width: '100%'}}
+                            hintText="Улица"
+                            floatingLabelText="Улица"
+                            ref="street"
+                            value={this.state.street}
+                            onChange={this._refresh}/>
+                    </div>
                 </Card>
             </div>
-            <div style={{width: '100%'}}>
+            <div style={{width: '100%', display: 'table'}}>
                 <Card style={{margin: '12px 12px 0 12px'}}>
-                    <TextField
-                        style={{margin: '0 12px 6px 12px', width: '95%'}}
-                        hintText="Дом"
-                        floatingLabelText="Дом"
-                        ref="home"
-                        value={this.state.home}
-                        onChange={this._refresh}/>
+                    <div style={{display: 'table-cell', padding: '0 9px 0 9px'}}>
+                        <FontIcon style={{verticalAlign: 'middle', fontSize: '20px'}}
+                                  color={settings.primaryColor}
+                                  className="material-icons">
+                            domain
+                        </FontIcon>
+                    </div>
+                    <div style={{display: 'table-cell', width: '95%'}}>
+                        <TextField
+                            style={{width: '100%'}}
+                            hintText="Дом"
+                            floatingLabelText="Дом"
+                            ref="home"
+                            value={this.state.home}
+                            onChange={this._refresh}/>
+                    </div>
                 </Card>
             </div>
-            <div style={{width: '100%'}}>
+            <div style={{width: '100%', display: 'table'}}>
                 <Card style={{margin: '12px 12px 0 12px'}}>
-                    <TextField
-                        style={{margin: '0 12px 6px 12px', width: '95%'}}
-                        hintText="Квартира"
-                        floatingLabelText="Квартира"
-                        ref="flat"
-                        value={this.state.flat}
-                        onChange={this._refresh}/>
+                    <div style={{display: 'table-cell', padding: '0 9px 0 9px'}}>
+                        <FontIcon style={{verticalAlign: 'middle', fontSize: '20px'}}
+                                  color={settings.primaryColor}
+                                  className="material-icons">
+                            store_mall_directory
+                        </FontIcon>
+                    </div>
+                    <div style={{display: 'table-cell', width: '95%'}}>
+                        <TextField
+                            style={{width: '100%'}}
+                            hintText="Квартира"
+                            floatingLabelText="Квартира"
+                            ref="flat"
+                            value={this.state.flat}
+                            onChange={this._refresh}/>
+                    </div>
                 </Card>
             </div>
         </div>;
