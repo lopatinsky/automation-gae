baec3c39201876fba47b5b87761adeb28004dabc
dvpermyakov <dvpermyakov1@gmail.com>
2015-11-02 18:40:05 +0300

    update empty cat pic

--- frontend-src/app/scripts/settings.js
+++ frontend-src/app/scripts/settings.js
@@ -2,7 +2,7 @@ import Colors from 'material-ui/lib/styles/colors';
 
 const settings = {
     primaryColor:  Colors.red500,
-    defaultCategoryPic: 'http://thumbs.dreamstime.com/t/fast-food-icons-8742935.jpg'
+    defaultCategoryPic: 'https://slack-files.com/files-tmb/T02E6DU7C-F0DLN6PJA-0d8a0ea4e3/noimage_icon_3x____________360.png'
 };
 
 export default settings;
