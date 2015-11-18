042db7268014d1ab7ca3541f248502d8da885269
dvpermyakov <dvpermyakov1@gmail.com>
2015-11-18 19:39:30 +0300

    clear

--- handlers/maintenance/automation.py
+++ handlers/maintenance/automation.py
@@ -1,12 +1,9 @@
 # coding:utf-8
 from google.appengine.api.namespace_manager import namespace_manager
-from google.appengine.ext import ndb
 from google.appengine.ext.ndb import metadata
-from models import MenuItem, MenuCategory
 
 from models.config.config import Config
 from models.config.version import CURRENT_VERSION, CURRENT_APP_ID
-from models.proxy.doubleb import DoublebCompany
 
 __author__ = 'dvpermyakov'
 
@@ -15,12 +12,6 @@ from base import BaseHandler
 
 class CompaniesListHandler(BaseHandler):
     def get(self):
-        namespace_manager.set_namespace('academia')
-        ids = [4697455123759104]
-        for id in ids:
-            category = MenuCategory.get_by_id(id)
-            if category:
-                category.key.delete()
         namespaces = []
         for namespace in metadata.get_namespaces():
             namespace_manager.set_namespace(namespace)
