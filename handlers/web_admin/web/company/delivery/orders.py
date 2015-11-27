70c020f9cc205a02ecee872965991e4cd9e6a19f
dvpermyakov <dvpermyakov1@gmail.com>
2015-11-27 18:05:24 +0300

    add modifiers price for delivery page

--- handlers/web_admin/web/company/delivery/orders.py
+++ handlers/web_admin/web/company/delivery/orders.py
@@ -78,13 +78,15 @@ def order_items_values(order):
         for modifier_dict in item_dict['single_modifiers']:
             modifier = SingleModifier.get_by_id(int(modifier_dict['id']))
             modifier.amount = modifier_dict['quantity']
-            item_obj.total_float_price += modifier.float_price * modifier.amount * item_obj.amount
+            modifier.total_float_price = modifier.float_price * modifier.amount
+            item_obj.total_float_price += modifier.total_float_price * item_obj.amount
             item_obj.modifiers.append(modifier)
         for modifier_dict in item_dict['group_modifiers']:
             modifier = GroupModifier.get_by_id(int(modifier_dict['id']))
             choice = modifier.get_choice_by_id(int(modifier_dict['choice']))
             choice.amount = modifier_dict['quantity']
-            item_obj.total_float_price += choice.float_price * choice.amount * item_obj.amount
+            choice.total_float_price = choice.float_price * choice.amount
+            item_obj.total_float_price += choice.total_float_price * item_obj.amount
             item_obj.modifiers.append(choice)
         if gift:
             item_obj.total_float_price = 0.0
