07a9714f7868e31cd7cba315c23f0be5994bcf1e
Mikhail Burshteyn <mdburshteyn@gmail.com>
2016-01-08 15:52:28 +0300

    Allow thread-local config to work when namespace changes

--- models/config/local.py
+++ models/config/local.py
@@ -1,18 +1,23 @@
 import threading
 
+from google.appengine.api import namespace_manager
+
 __author__ = 'dvpermyakov'
 
 
 class LocalConfigProxy(object):
     _local = threading.local()
 
+    def __init__(self):
+        self._local.config = None
+
     @property
     def _config_object(self):
         from models.config.config import Config
-        try:
-            self._local.config
-        except AttributeError:
-            self._local.config = Config.get()
+        if self._local.config:
+            if self._local.config.key.namespace() == namespace_manager.get_namespace():
+                return self._local.config
+        self._local.config = Config.get()
         return self._local.config
 
     def __getattr__(self, item):
