a6d42f347abcc87b4df71eec3c20ff5e93f59251
Mikhail Burshteyn <mdburshteyn@gmail.com>
2016-02-01 15:48:44 +0300

    Auto-create courier card payment type

--- handlers/web_admin/web/company/payment_type.py
+++ handlers/web_admin/web/company/payment_type.py
@@ -1,8 +1,7 @@
 from methods.auth import payment_types_rights_required
 from models import PaymentType, STATUS_UNAVAILABLE, STATUS_AVAILABLE
 from base import CompanyBaseHandler
-from models.payment_types import CASH_PAYMENT_TYPE, CARD_PAYMENT_TYPE, PAYPAL_PAYMENT_TYPE
-
+from models.payment_types import CASH_PAYMENT_TYPE, CARD_PAYMENT_TYPE, PAYPAL_PAYMENT_TYPE, CARD_COURIER_PAYMENT_TYPE
 
 __author__ = 'dvpermyakov'
 
@@ -19,6 +18,9 @@ class PaymentTypesHandler(CompanyBaseHandler):
         paypal = PaymentType.get_by_id(str(PAYPAL_PAYMENT_TYPE))
         if not paypal:
             PaymentType(id=str(PAYPAL_PAYMENT_TYPE), title='paypal', status=STATUS_UNAVAILABLE).put()
+        courier_card = PaymentType.get_by_id(str(CARD_COURIER_PAYMENT_TYPE))
+        if not courier_card:
+            PaymentType(id=str(CARD_COURIER_PAYMENT_TYPE), title='courier card', status=STATUS_UNAVAILABLE).put()
         self.render('/payment_types.html', payments=PaymentType.query().fetch(),
                     CARD_PAYMENT_TYPE=str(CARD_PAYMENT_TYPE))
 
