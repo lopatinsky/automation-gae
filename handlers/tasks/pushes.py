b7eebe09fa630cb05a5d374196e1be1ac60e3860
Artem <temi4hik@gmail.com>
2016-03-02 20:07:57 +0300

    no message

--- handlers/tasks/pushes.py
+++ handlers/tasks/pushes.py
@@ -1,12 +1,14 @@
 from datetime import datetime
+
 from webapp2 import RequestHandler
+
+from methods.push import send_review_push
 from models import Notification
-from models.push import *
-from models.specials import STATUS_CREATED
-from models.push import ReviewPush
-from methods.push import send_multichannel_push, send_review_push
+# from models.push import *
+from models.push import SimplePush
+from models.specials import STATUS_CREATED, ReviewPush
+from models.push import ReviewPush as RP
 from models.client import IOS_DEVICE, ANDROID_DEVICE
-import logging
 
 __author__ = 'dvpermyakov'
 
@@ -27,8 +29,8 @@ class StartPushesHandler(RequestHandler):
                                       channels=channels, device_type=ANDROID_DEVICE)
 
             ios_push = SimplePush(text=notification.text, should_popup=notification.should_popup,
-                                      full_text=notification.popup_text, header=notification.header,
-                                      channels=channels, device_type=IOS_DEVICE)
+                                  full_text=notification.popup_text, header=notification.header,
+                                  channels=channels, device_type=IOS_DEVICE)
 
             android_push.send()
             ios_push.send()
@@ -39,14 +41,18 @@ class StartPushesHandler(RequestHandler):
 class SendPushReviewHandler(RequestHandler):
     def post(self):
         review_id = self.request.get_range('review_id')
+
+
         review = ReviewPush.get_by_id(review_id)
         if not review:
             self.abort(400)
         order = review.order.get()
 
-        push = ReviewPush(order, 0)
+        # push = ReviewPush(order, 0)
+        # push.send()
+        push = RP(order, 0)
         push.send()
 
-        send_review_push(order)
-        review.sent = datetime.datetime.utcnow()
+        # send_review_push(order)
+        review.sent = datetime.utcnow()
         review.put()
